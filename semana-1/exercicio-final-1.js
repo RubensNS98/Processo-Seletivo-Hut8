11dce4b5ef75e141fa89423789577338f75986cb
Rubens Santos <88848638+RubensNS98@users.noreply.github.com>
2022-07-29 09:48:41 -0300

    Add files via upload

---
+++ semana-1/exercicio-final-1.js
@@ -0,0 +1,17 @@
+//Exercícios Finais
+//Exercício 1
+
+//Atribuição da variável
+let number = 12;
+
+//Checagem do número
+function checkPar(number) {
+    if (number % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+//Impressão na tela
+console.log(checkPar(number));
---
+++ semana-1/exercicio-final-2.js
@@ -0,0 +1,18 @@
+//Exercícios Finais
+//Exercício 2
+
+//Atribuição da variável
+let age = 11
+
+//Comparação através de condicionais com o uso de expressões lógicas
+if (age >= 5 && age <= 7) {
+  console.log("Infantil A")
+} else if (age >= 8 && age <= 10){
+  console.log("Infantil B")
+} else if (age >= 11 && age <= 13){ 
+  console.log("Juvenil A")
+} else if (age >= 14 && age <= 17){
+  console.log("Juvenil B")
+} else if (age >= 18){
+  console.log("Adulto")
+}
